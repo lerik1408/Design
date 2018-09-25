72103a8c670809ee5e758bce549e5ca1bd76dc34
iliia <popruzhuk.38@gmail.com>
2018-09-25 21:19:53 +0200

    9 day

--- script/slider.js
+++ script/slider.js
@@ -5,6 +5,7 @@ window.addEventListener('DOMContentLoaded', function() {
   dotsWrap = document.querySelector('.slider-dots'),
   dots = document.getElementsByClassName('dot');
   console.log(slides);
+  console.log("ggggdg")
   console.log(dotsWrap);
   console.log(dots);
   console.log(dots.length)
@@ -40,5 +41,38 @@ window.addEventListener('DOMContentLoaded', function() {
         currentSlide(i);
       }
     }
-  });
+  }); 
+  // Слайдер1
+  // Слайдер дивов
+  let PortfolioIndex = 2,
+  portfolio=document.getElementsByClassName("portfolio-slider-item"),
+  PortfolioDotWrap=document.querySelector('.portfolio-menu'),
+  PortfolioDot=document.getElementsByClassName('portfolio-menu-dot');
+  console.log(PortfolioDotWrap);
+  console.log(portfolio);
+  showPortfolio(PortfolioIndex);
+  function showPortfolio(index){
+    if(index>portfolio.length){
+      PortfolioIndex =1;
+    }
+    if(index<0){
+      PortfolioIndex=portfolio.length;
+    }
+    for(let i=0;i<portfolio.length;i++){
+      portfolio[i].style.display="none";
+      PortfolioDot[i].classList.remove('portfolio-menu-dot-active');
+    }
+    portfolio[PortfolioIndex-1].style.display="block";
+    PortfolioDot[PortfolioIndex-1].classList.add('portfolio-menu-dot-active');
+  }
+  function currentPortfolio(index){
+    showPortfolio(PortfolioIndex = index)
+  }
+  PortfolioDotWrap.addEventListener('click',function(event){
+    for (let i = 0; i <= PortfolioDot.length; i++){
+      if(event.target.classList.contains('portfolio-menu-dot') && event.target == PortfolioDot[i-1]){
+        currentPortfolio(i);
+      }
+    }
+  })
 });
